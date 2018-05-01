b3037a85e8d27bccb192491989c1c918057a0894
Subsight <38530980+Subsight@users.noreply.github.com>
2018-05-01 12:06:33 -0400

    Add files via upload

--- combat.py
+++ combat.py
@@ -1,39 +1,39 @@
-def combat(NumOfEnemies):
-    for c in range (NumOfEnemies):
-        print("A wild",eList[c].name,"appears!")
-        print("---------------------")
-        print("hero stats:")
-        print("---------------------")
-        hero.showStats()
-        print("---------------------")
-        print("enemy stats:")
-        print("---------------------")
-        eList[c].getStats()
-        if eList[c].SPD > hero.SPD:
-            curTurn = "Enemy"
-        elif hero.SPD > eList[c].SPD:
+def combat(enemy,player):
+    print("An enemy",enemy.name,"appears!")
+    print("---------------------")
+    print("hero stats:")
+    print("---------------------")
+    player.getStats()
+    print("~~~~~~~~~~~~~~~~~~~~~")
+    print("enemy stats:")
+    print("---------------------")
+    enemy.getStats()
+    if enemy.SPD > player.SPD:
+        curTurn = "Enemy"
+    elif player.SPD > enemy.SPD:
+        curTurn = "Player"
+    else:
+        if random.randint(1,2) == 1:
             curTurn = "Player"
         else:
-            if random.randint(1,2) == 1:
-                curTurn = "Player"
-            else:
-                curTurn = "Enemy"
-        fled = False
-        while hero.HP > 0 and eList[c].HP > 0 and not fled:
-            if curTurn == "Player":
-                print(">Physical    >Flee    >Energy    >Item")
-                selection = ""
-                while(selection not in ("Physical","Flee","Energy","Item")):
-                    selection = input("-->")
-                    if selection == "Physical":
-                        print("you attack",eList[c].name)
-                        eList[c].takePDamage(hero.PHY)
-                    elif selection == "Energy":
-                        print("you use energy weapons on",eList[c].name)
-                        eList[c].takeEDamage(hero.ENG)
-                    elif selection == "Flee":
+            curTurn = "Enemy"
+    fled = False
+    while player.HP > 0 and enemy.HP > 0 and not fled:
+        if curTurn == "Player":
+            print(">Physical    >Energy    >Flee    >Item")
+            selection = ""
+            while(selection not in ("Physical","Energy","Flee","Item")):
+                selection = input("-->")
+                if selection == "Physical":
+                    print("you attack with physical weapons",enemy.name)
+                    enemy.takePDamage(player.PHY)
+                elif selection == "Energy":
+                    print("you use energy weapons on",enemy.name)
+                    enemy.takeEDamage(player.ENG)
+                elif selection == "Flee":
+                    if enemy.SPD != 1337:
                         roll = random.randint(1,100)
-                        if hero.SPD > eList[c].SPD:
+                        if player.SPD > enemy.SPD:
                             fleeChance = 90
                         else:
                             fleeChance = 50
@@ -44,29 +44,42 @@ def combat(NumOfEnemies):
                             fled = True
                             time.sleep(1)
                         else:
-                            print("faild to flee")
+                            print("failed to flee")
                             time.sleep(1)
-                    elif selection == "Item":
-                        pass
-                        #list of usable items
-                        #use "while selection not in" to make sure input is an item
-                        #activate item buff
-                        #remove item from inventory
-                    curTurn = "Enemy"
-            elif curTurn == "Enemy":
-                if eList[c].PHY >= eList[c].ENG:
-                    print("The",eList[c].name,"attacks!")
-                    hero.takePDamage(eList[c].PHY)
-                elif eList[c].PHY < eList[c].ENG:
-                    print("The",eList[c].name,"uses energy weapons!")
-                    hero.takeEDamage(eList[c].ENG)
+                    else:
+                        print("failed to flee")
+                        time.sleep(1)
+                elif selection == "Item":
+                    pass
+                    #list of usable items
+                    #use "while selection not in" to make sure input is an item
+                    #activate item buff
+                    #remove item from inventory
+                curTurn = "Enemy"
+        elif curTurn == "Enemy":
+            atType = random.tandint(1,100)
+            if enemy.PHY >= enemy.ENG:
+                if atType > 10:
+                    print("The",enemy.name,"attacks!")
+                    player.takePDamage(enemy.PHY)
                 else:
-                    print("An Error Occured...")
-                curTurn = "Player"
-        hero.encounter += 1
-        if hero.HP <= 0:
-            print("-----------------")
-            print("you died")
-            print("-----------------")
-            break
-
+                    print("The",enemy.name,"uses energy weapons!")
+                    player.takeEDamage(enemy.ENG)
+            elif enemy.PHY < enemy.ENG:
+                if atType > 10:
+                    print("The",enemy.name,"uses energy weapons!")
+                    player.takeEDamage(enemy.ENG)
+                else:
+                    print("The",enemy.name,"attacks!")
+                    player.takePDamage(enemy.PHY)
+            else:
+                print("An Error Occured...")
+            curTurn = "Player"
+    if player.HP <= 0:
+        print("-----------------")
+        print("you died")
+        print("-----------------")
+    elif fled == True:
+        pass
+    else:
+        player.encounter += 1
